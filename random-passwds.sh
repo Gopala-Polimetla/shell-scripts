9bf0fddb74224876d159bf742cafa32032b149cb
root <root@ip-172-31-37-19.ap-south-1.compute.internal>
2023-01-31 06:52:28 +0000

    script done

--- random-passwds.sh
+++ random-passwds.sh
@@ -17,3 +17,15 @@ echo "$PASSWORD"
 # Use the nanoseconds to act as randomization.
 PASSWORD=$(date +%s%N)
 echo "$PASSWORD"
+
+# A better password.
+PASSWORD=$(date +%s%N | sha256sum | head -c32)
+echo "$PASSWORD"
+
+# Even a better password.
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c48)
+echo "${PASSWORD}"
+
+# Append a special character to the password.
+SPECIAL_CHARACTER=$(echo '!@#$%^&*()_+=-' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHARACTER}"
